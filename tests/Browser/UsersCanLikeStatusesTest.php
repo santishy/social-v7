ec96581758b8c883fa2bc6ae701bceaff7dcf419
santiago martin <santiagomartinochoaestrada@gmail.com>
2020-11-10 12:57:17 -0600

    ...

--- tests/Browser/UsersCanLikeStatusesTest.php
+++ tests/Browser/UsersCanLikeStatusesTest.php
@@ -18,9 +18,9 @@ class UsersCanLikeStatusesTest extends DuskTestCase
         $status = factory(Status::class)->create();
         $this->browse(function (Browser $browser) use ($status) {
             $browser->visit('/')
-                    ->waitForText($status->body)
-                    ->press('@like-btn')
-                    ->assertPathIs('/login');
+                ->waitForText($status->body)
+                ->press('@like-btn')
+                ->assertPathIs('/login');
         });
     }
     /**
@@ -31,20 +31,20 @@ class UsersCanLikeStatusesTest extends DuskTestCase
         $user = factory(User::class)->create();
         $status = factory(Status::class)->create();
 
-        $this->browse(function (Browser $browser) use ($user,$status) {
+        $this->browse(function (Browser $browser) use ($user, $status) {
             $browser->loginAs($user)
-                    ->visit('/')
-                    ->waitForText($status->body)
-                    ->assertSee($status->body)
-                    ->assertSeeIn('@likes-count',0)
-                    ->press('@like-btn')
-                    ->waitForText('Te gusta')
-                    ->assertSee('Te gusta')
-                    ->assertSeeIn('@likes-count',1)
-                    ->press('@like-btn')
-                    ->waitForText('Me gusta')
-                    ->assertSee('Me gusta')
-                      ->assertSeeIn('@likes-count',0);
+                ->visit('/')
+                ->waitForText($status->body)
+                ->assertSee($status->body)
+                ->assertSeeIn('@likes-count', 0)
+                ->press('@like-btn')
+                ->waitForText('Te gusta')
+                ->assertSee('Te gusta')
+                ->assertSeeIn('@likes-count', 1)
+                ->press('@like-btn')
+                ->waitForText('Me gusta')
+                ->assertSee('Me gusta')
+                ->assertSeeIn('@likes-count', 0);
         });
     }
     /**
@@ -55,17 +55,17 @@ class UsersCanLikeStatusesTest extends DuskTestCase
         $user = factory(User::class)->create();
         $status = factory(Status::class)->create();
 
-        $this->browse(function (Browser $browser1,Browser $browser2) use ($user,$status) {
+        $this->browse(function (Browser $browser1, Browser $browser2) use ($user, $status) {
             $browser1->visit('/');
             $browser2->loginAs($user)
-                    ->visit('/')
-                    ->waitForText($status->body)
-                    ->assertSee($status->body)
-                    ->assertSeeIn('@likes-count',0)
-                    ->press('@like-btn')
-                    ->waitForText('Te gusta');
-            
-            $browser1->assertSeeIn('@likes-count',1);
+                ->visit('/')
+                ->waitForText($status->body)
+                ->assertSee($status->body)
+                ->assertSeeIn('@likes-count', 0)
+                ->press('@like-btn')
+                ->waitForText('Te gusta');
+
+            $browser1->assertSeeIn('@likes-count', 1);
         });
     }
 }
